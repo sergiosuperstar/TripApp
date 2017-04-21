5fff0b5ca2d4bf41038a17f5acfe1cbed6d5f9bd
NNemanjaMM <nemanja.miladinovic@live.com>
2017-04-21 23:43:50 +0200

    client: "Ticket Purchase" activity UI created

---
+++ src/client/app/src/main/java/com/example/icf/tripappclient/TicketPurchaseVertical.java
@@ -0,0 +1,21 @@
+package com.example.icf.tripappclient;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class TicketPurchaseVertical extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ticket_purchase_vertical, container, false);
+
+    }
+}
