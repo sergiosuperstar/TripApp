d00b7b289d863dff8a637dcf81678836f28e90c0
vuletic <vuletic93@gmail.com>
2017-04-24 00:27:10 +0200

    Ticket info activity created

---
+++ src/client/app/src/main/java/com/example/icf/tripappclient/TicketInfo.java
@@ -0,0 +1,13 @@
+package com.example.icf.tripappclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TicketInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ticket_info);
+    }
+}
