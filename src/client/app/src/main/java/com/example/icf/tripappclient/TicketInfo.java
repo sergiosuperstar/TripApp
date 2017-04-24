98ec8e586d2a3fe50dc94bbd2bd067be47fd43c9
vuletic <vuletic93@gmail.com>
2017-04-24 12:54:24 +0200

    Started working on toolbar and nav menu

--- src/client/app/src/main/java/com/example/icf/tripappclient/TicketInfo.java
+++ src/client/app/src/main/java/com/example/icf/tripappclient/TicketInfo.java
@@ -2,6 +2,7 @@ package com.example.icf.tripappclient;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class TicketInfo extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class TicketInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ticket_info);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
