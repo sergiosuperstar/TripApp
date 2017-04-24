168fe8cdb7467e5b9dd845a2039bb49ea701a924
Sergej Keselj <keshelj@hotmail.com>
2017-04-24 16:12:56 +0200

    implement main activity buttons and connect with buy tickets activity

--- src/client/app/src/main/java/com/example/icf/tripappclient/MainActivity.java
+++ src/client/app/src/main/java/com/example/icf/tripappclient/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.icf.tripappclient;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user touches the button */
+    public void buyOneHourTicket(View view) {
+        Intent intent = new Intent(getApplicationContext(), TicketPurchase.class);
+        startActivity(intent);
+    }
+
+    public void buyOneDayTicket(View view) {
+        Intent intent = new Intent(getApplicationContext(), TicketPurchase.class);
+        startActivity(intent);
+    }
+
+    public void buyWeekDayTicket(View view) {
+        Intent intent = new Intent(getApplicationContext(), TicketPurchase.class);
+        startActivity(intent);
+    }
+
+    public void buyMonthDayTicket(View view) {
+        Intent intent = new Intent(getApplicationContext(), TicketPurchase.class);
+        startActivity(intent);
     }
 }
