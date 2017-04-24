78d26f6ff991a18ea6461b475ece03218690223f
vuletic <vuletic93@gmail.com>
2017-04-24 16:32:13 +0200

    Navigation drawer update

--- src/client/app/src/main/java/com/example/icf/tripappclient/MainActivity.java
+++ src/client/app/src/main/java/com/example/icf/tripappclient/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.icf.tripappclient;
 
+import android.app.Activity;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String[] mPlanetTitles;
-    private DrawerLayout mDrawerLayout;
-    private ListView mDrawerList;
+    private ActionBarDrawerToggle toggle;
+    private Activity act = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setNavigationIcon(R.drawable.menu_alt);
+
         setSupportActionBar(toolbar);
+        final DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.buy_ticket, R.string.app_name);
+        toggle.setDrawerIndicatorEnabled(true);
+
+        drawerLayout.addDrawerListener(toggle);
     }
+
+
 }
