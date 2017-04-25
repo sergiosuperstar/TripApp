37676f70a54325d431b3d4a9e8641bdbe790a296
NNemanjaMM <nemanja.miladinovic@live.com>
2017-04-25 12:05:59 +0200

    client: Login activity created

--- src/client/app/src/main/java/com/example/icf/tripappclient/fragments/TicketPurchaseVertical.java
+++ src/client/app/src/main/java/com/example/icf/tripappclient/fragments/TicketPurchaseVertical.java
@@ -1,4 +1,4 @@
-package com.example.icf.tripappclient;
+package com.example.icf.tripappclient.fragments;
 
 import android.content.Context;
 import android.net.Uri;
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.icf.tripappclient.R;
 
 
 public class TicketPurchaseVertical extends Fragment {
