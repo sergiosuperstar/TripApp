fe04422e236ed32f9cf862f0edc4c1f22ff58efa
vuletic <vuletic93@gmail.com>
2017-05-03 17:41:27 +0200

    Dynamic qr code generation

--- src/client/app/src/main/java/com/example/icf/tripappclient/fragments/TicketInfo.java
+++ src/client/app/src/main/java/com/example/icf/tripappclient/fragments/TicketInfo.java
@@ -1,12 +1,25 @@
 package com.example.icf.tripappclient.fragments;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.icf.tripappclient.R;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+
+import static android.graphics.Color.BLACK;
+import static android.graphics.Color.WHITE;
+import static android.graphics.Color.YELLOW;
 
 /**
  * Created by Vuletic on 28.4.2017.
@@ -21,9 +34,42 @@ public class TicketInfo extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ticket_info, container, false);
+
+        View view=inflater.inflate(R.layout.fragment_ticket_info, container, false);
+
+        ImageView imageView = (ImageView) view.findViewById(R.id.qrExample);
+
+        try {
+            Bitmap bitmap = encodeAsBitmap("Informacije o karti; 22.14.2017.; 1234156; Ovo je to to !; Ime-prezime");
+            imageView.setImageBitmap(bitmap);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+
+        return view;
     }
 
+    Bitmap encodeAsBitmap(String str) throws WriterException {
+        BitMatrix result;
+        try {
+            result = new MultiFormatWriter().encode(str,
+                    BarcodeFormat.QR_CODE, 150, 150, null);
+        } catch (IllegalArgumentException iae) {
+            // Unsupported format
+            return null;
+        }
+        int w = result.getWidth();
+        int h = result.getHeight();
+        int[] pixels = new int[w * h];
+        for (int y = 0; y < h; y++) {
+            int offset = y * w;
+            for (int x = 0; x < w; x++) {
+                pixels[offset + x] = result.get(x, y) ? BLACK : WHITE;
+            }
+        }
+        Bitmap bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        bitmap.setPixels(pixels, 0, 150, 0, 0, w, h);
+        return bitmap;
+    }
 
 }
