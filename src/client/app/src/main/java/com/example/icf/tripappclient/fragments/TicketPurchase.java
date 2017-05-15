7c357d2cf4c791c2653f60ab94410e59cfe93914
NNemanjaMM <nemanja.miladinovic@live.com>
2017-05-15 13:57:26 +0200

    client: Settings UI and Purchase behaviour updated

--- src/client/app/src/main/java/com/example/icf/tripappclient/fragments/TicketPurchase.java
+++ src/client/app/src/main/java/com/example/icf/tripappclient/fragments/TicketPurchase.java
@@ -6,6 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.icf.tripappclient.R;
 
@@ -15,8 +18,10 @@ import com.example.icf.tripappclient.R;
 
 public class TicketPurchase extends Fragment {
 
+    private double ticketPrice;
+
     public TicketPurchase() {
-        // Required empty public constructor
+        ticketPrice = 6.99;
     }
 
     @Override
@@ -25,8 +30,34 @@ public class TicketPurchase extends Fragment {
 
         ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle(R.string.ticket_purchase_title);
 
+
         return inflater.inflate(R.layout.fragment_ticket_purchase, container, false);
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
 
+        TextView priceText = (TextView) getView().findViewById(R.id.priceValue);
+        priceText.setText(Double.toString(ticketPrice));
+
+        //View view=inflater.inflate(R.layout.fragment_ticket_purchase, container, false);
+        final Spinner countSpinner = (Spinner) getView().findViewById(R.id.numberValue);
+        final TextView totalText = (TextView) getView().findViewById(R.id.totalValue);
+
+        countSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                int count = Integer.parseInt(countSpinner.getSelectedItem().toString());
+                String totalString = Double.toString(count * ticketPrice);
+                totalText.setText(totalString);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+
+        });
+    }
 }
