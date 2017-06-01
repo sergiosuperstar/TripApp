8943ef41a8eb5924cbe0d843b2d9a54cc774ae3b
NNemanjaMM <nemanja.miladinovic@live.com>
2017-06-01 20:49:49 +0200

    client: added scrollable view where needed
    Login fragment removed, Ticket info and Ticket purchase have scrollable
    parts

--- src/client/app/src/main/java/com/example/icf/tripappclient/fragments/LoginVertical.java
+++ src/client/app/src/main/java/com/example/icf/tripappclient/fragments/LoginVertical.java
@@ -19,12 +19,4 @@ public class LoginVertical extends Fragment {
         // Required empty public constructor
     }
 
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login_vertical, container, false);
-    }
-
 }
