21b69183de160638f79f6d945d30053cb043016b
NNemanjaMM <nemanja.miladinovic@live.com>
2017-06-06 20:25:23 +0200

    client: tickets list on home page converted to adapter

--- src/client/app/src/main/java/com/example/icf/tripappclient/fragments/Home.java
+++ src/client/app/src/main/java/com/example/icf/tripappclient/fragments/Home.java
@@ -8,11 +8,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
 import com.example.icf.tripappclient.R;
+import com.example.icf.tripappclient.adapters.PurchaseAdapter;
+
+import java.util.List;
+
+import io.swagger.client.model.TicketType;
 
 
 public class Home extends Fragment {
+
+    private List<TicketType> ticketTypes;
+    private GridView ticketsDisplay;
 
     public Home() {
         // Required empty public constructor
@@ -23,6 +32,9 @@ public class Home extends Fragment {
                              Bundle savedInstanceState) {
 
         ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle(R.string.home_title);
+
+//        this.ticketsDisplay = (GridView) ((AppCompatActivity)getActivity()).findViewById(R.id.tickets_grid);
+//        this.ticketsDisplay.setAdapter(new PurchaseAdapter((AppCompatActivity)getActivity(), ticketTypes));
 
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
